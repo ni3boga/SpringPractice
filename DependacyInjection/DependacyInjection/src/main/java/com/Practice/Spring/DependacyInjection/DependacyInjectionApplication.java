5d07901eb54c95a4303cf41eced7d1953c40bf91
Nitin Boga <Ni3boga@gmail.com>
2024-05-22 19:04:26 +0530

    initial Commit

---
+++ DependacyInjection/DependacyInjection/src/main/java/com/Practice/Spring/DependacyInjection/DependacyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.Practice.Spring.DependacyInjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependacyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependacyInjectionApplication.class, args);
+	}
+
+}
---
+++ DependacyInjection/DependacyInjection/src/test/java/com/Practice/Spring/DependacyInjection/DependacyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practice.Spring.DependacyInjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependacyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
